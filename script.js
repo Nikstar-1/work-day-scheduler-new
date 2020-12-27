d9a85e94dec818d025684de5232dc33efd7564c9
Nikstar-1 <rk_787@hotmail.co.uk>
2020-12-27 14:41:40 +0000

    Add functionality to get previous inputs from local storage

--- script.js
+++ script.js
@@ -11,5 +11,22 @@ $(document).ready(function(){
 
 
 
-
+var localStorage9am = localStorage.getItem("hour-9");
+$("#hour-9 .description").val(localStorage9am); 
+var localStorage9am = localStorage.getItem("hour-10");
+$("#hour-10 .description").val(localStorage9am); 
+var localStorage9am = localStorage.getItem("hour-11");
+$("#hour-11 .description").val(localStorage9am); 
+var localStorage9am = localStorage.getItem("hour-12");
+$("#hour-12 .description").val(localStorage9am); 
+var localStorage9am = localStorage.getItem("hour-13");
+$("#hour-13 .description").val(localStorage9am); 
+var localStorage9am = localStorage.getItem("hour-14");
+$("#hour-14 .description").val(localStorage9am); 
+var localStorage9am = localStorage.getItem("hour-15");
+$("#hour-15 .description").val(localStorage9am); 
+var localStorage9am = localStorage.getItem("hour-16");
+$("#hour-16 .description").val(localStorage9am); 
+var localStorage9am = localStorage.getItem("hour-17");
+$("#hour-17 .description").val(localStorage9am); 
 })
