4a1d1474c2c268ef415c1ed0e1b80711607249e4
Nikstar-1 <rk_787@hotmail.co.uk>
2020-12-27 14:36:45 +0000

    Add functionality where input added to local storage

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
 
     var currentDateAndTime = moment();
     $("#currentDay").text(currentDateAndTime); 
-
+ $(".saveBtn").on("click", function(){
+     var timeId = $(this).parent().attr("id"); 
+     var userInput = $(this).siblings(".description").val(); 
+     localStorage.setItem(timeId, userInput)
+ })
 
 
 
